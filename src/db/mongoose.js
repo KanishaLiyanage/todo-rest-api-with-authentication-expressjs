cca8d5f88e9b11e87d86ed88d45f9684a9976656
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-08-14 18:16:22 +0530

    db url changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-// const url = process.env.MongoDBAtlas;
+const url = 'mongodb+srv://KanishaL:kanisha123@cluster0.yttgw1l.mongodb.net/task-manager-api?retryWrites=true&w=majority';
 
-mongoose.connect('mongodb+srv://KanishaL:kanisha123@cluster0.yttgw1l.mongodb.net/task-manager-api?retryWrites=true&w=majority');
+mongoose.connect(url);
