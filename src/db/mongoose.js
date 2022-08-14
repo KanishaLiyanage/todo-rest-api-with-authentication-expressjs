8e3c9a9cedcbf504317b4f27680dcdb5ff88020c
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-08-14 12:11:49 +0530

    db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 
 // const url = process.env.MongoDBAtlas;
 
-mongoose.connect('mongodb+srv://KanishaL:kanisha123@cluster0.yttgw1l.mongodb.net/?retryWrites=true&w=majority/task-manager-api');
+mongoose.connect('mongodb+srv://KanishaL:kanisha123@cluster0.yttgw1l.mongodb.net/task-manager-api?retryWrites=true&w=majority');
