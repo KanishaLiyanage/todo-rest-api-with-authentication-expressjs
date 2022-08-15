7b9428d2b36d678980b8b641e067c22e9bd65398
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-08-15 11:29:08 +0530

    commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//const dbName;
 const url = 'mongodb+srv://KanishaL:kanisha123@cluster0.yttgw1l.mongodb.net/task-manager-api?retryWrites=true&w=majority';
 
 mongoose.connect(url);
